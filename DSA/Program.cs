9098c39c4fc39dffc3eb4d88b1e845f045ce14eb
Arif Mahmud <k.i.d.s69@outlook.com>
2025-07-15 01:47:37 +0600

    Binarry Search algorithm added

--- DSA/Program.cs
+++ DSA/Program.cs
@@ -1,10 +1,57 @@
 ﻿namespace DSA
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Enter the length of the arrat: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[n];
+            Console.WriteLine();
+
+            Console.Write("Enter the elements of the array:");
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine();
+            Console.Write("Enter the element to be searched: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            // Binary Search Algorithm
+            Array.Sort(arr); // Ensure the array is sorted before performing binary search
+
+
+            int left = 0, right = n - 1;
+            int index = -1;
+
+
+            while (left <= right)
+            {
+                int mid = (left + right) / 2;
+                if (arr[mid] == x)
+                {
+                    index = mid;
+                    break;
+                }
+                else if (arr[mid] < x)
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+
+            }
+            if (index != -1)
+            {
+                Console.WriteLine("Element found at index: " + index);
+            }
+            else
+            {
+                Console.WriteLine("Element not found in the array.");
+            }
         }
     }
 }
